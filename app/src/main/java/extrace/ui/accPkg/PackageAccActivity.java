88657104427d1b2c1c3c3117925b0d37a56258ff
lyy-123456 <56285831+lyy-123456@users.noreply.github.com>
2020-04-20 17:58:45 +0800

    第7次提交

--- app/src/main/java/extrace/ui/accPkg/PackageAccActivity.java
+++ app/src/main/java/extrace/ui/accPkg/PackageAccActivity.java
@@ -29,12 +29,12 @@ public class PackageAccActivity extends AppCompatActivity implements IDataAdapte
     private TextView pkg_textView;
     private Button pkg_acc_button;
     private Set<TransHistory> transHistorySet;
-    public final int PKG_NEW = 0;  //新建
-    public final int PKG_PACKED = 1; //已打包
-    public final int PKG_TRSNSIT = 2; //运输中
-    public final int PKG_ACCED = 3; //转运中心（已扫描）
-    public final int PKG_ACHIEVED = 4; //以达到achieved
-    public final int PKG_UNPACKED = 5;
+    public static final int PKG_NEW = 0;  //新建
+    public static final int PKG_PACKED = 1; //已打包
+    public static final int PKG_TRSNSIT = 2; //运输中
+    public static final int PKG_ACCED = 3; //转运中心（已扫描）
+    public static final int PKG_ACHIEVED = 4; //以达到achieved
+    public static final int PKG_UNPACKED = 5;
 
 
     TransPackageLoader transPackageLoader;
